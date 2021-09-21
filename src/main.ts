b86192beaa1fa47d05e92a55e736da9181d7024b
Ahmad Salih <45488156+salihcodev@users.noreply.github.com>
2021-09-21 12:19:44 +0200

    Successfully sending emails with nodejs. (#1)

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,16 @@ import cors from 'cors';
 import morgan from 'morgan';
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-
-// import utils:
+import nodemailer from 'nodemailer';
 
 // >>>> port that app is using:
 const PORT = process.env.PORT || 8080;
 
 // *******
-//
-//
 // >>>> INITIALIZING EXPRESS APP:
 const app = express();
 
 // *******
-//
 // >>>> APPLY THE MIDDLEWARES:
 // use cors
 app.use(cors());
@@ -29,7 +25,6 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('tiny'));
 // *******
 //
-//
 
 // >>>> SETUP ROUTES:
 // initial route:
@@ -39,9 +34,38 @@ app.get('/', (req: Request, res: Response): void => {
 
 // Email controller route:
 app.post('/contact', async (req: Request, res: Response): Promise<void> => {
-    const { email } = req.body
+    const { email: _emailToSend } = req.body;
 
-    res.send(`Email: ${email}`);
+    // >>>> SETUP NODEMAILER:
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: '@@#@gmail.com',
+                pass: '@#@$$#',
+            },
+        });
+
+        const mailOptions = {
+            from: '@@#@gmail.com',
+            to: _emailToSend,
+            subject: `Hello, ${_emailToSend.split("@")[0]}`,
+            html: '<h3>Another Email</h3> </br> <p>Some apps and devices use less secure sign-in technology</p>',
+        };
+
+        // send mail with defined transport object
+        const info = await transporter.sendMail(mailOptions);
+
+        res.status(200).json({
+            message: `Email sent successfully to ${_emailToSend}`,
+            emailId: info.messageId,
+        });
+    } catch (err) {
+        res.status(200).json({
+            message: `Failed to send the given email to: ${_emailToSend}`,
+            error: err,
+        });
+    }
 });
 
 app.listen(PORT, () =>
